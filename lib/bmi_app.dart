4cd64c5d186a17dddd43cf95478089bb0e838927
Shishir707 <mirshr8@gmail.com>
2025-11-13 10:59:22 +0600

    add all input fields & button

--- lib/bmi_app.dart
+++ lib/bmi_app.dart
@@ -12,25 +12,107 @@ class _BmiAppState extends State<BmiApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator',style: TextStyle(
-          fontSize: 25,
-          color: Colors.white,
-          fontWeight: FontWeight.w600
-        ),),
+        title: Text(
+          'BMI Calculator',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         backgroundColor: Colors.deepPurpleAccent,
         centerTitle: true,
       ),
       body: ListView(
         padding: EdgeInsets.all(16),
         children: [
+          Center(
+            child: Text(
+              'Know your health score with one tap',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
           TextField(
+            keyboardType: TextInputType.number,
             decoration: InputDecoration(
               labelText: 'Weight (KG)',
               border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10)
-              )
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
-          )
+          ),
+          SizedBox(height: 16),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Height (m)',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Height (cm)',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Height (feet)',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Height (inch)',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurpleAccent[200],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: Text(
+              'Show Result',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 17,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Result :',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
         ],
       ),
     );
